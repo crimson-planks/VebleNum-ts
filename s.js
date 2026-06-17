a2bc95a01c8861ddfa4ba1a2eeb164947f6f603e
crimson-planks <koreaminecraftmonster@gmail.com>
2026-06-17 22:00:24 +0900

    Update s.js

--- s.js
+++ s.js
@@ -1,6 +1,5 @@
-import VebleNum, {nonTransfiniteNumberArithmeticSymbol} from './built/index.js';
+import VebleNum from './built/index.js';
 window.VebleNum = VebleNum;
-window.nonTransfiniteNumberArithmeticSymbol = nonTransfiniteNumberArithmeticSymbol;
 window.solve = function solve() {
     let x = document.getElementById("exp").value;
     document.getElementById("out").innerHTML = VebleNum(x).toHTML();
